28601958a05b2a67ef5b8d893d6fae87546fd5d3
Casey Waldren <casey@nullspacevr.com>
2017-01-10 11:08:41 -0800

    Remove program from build temporarily

--- NSLoaderWrapper/Program.cs
+++ NSLoaderWrapper/Program.cs
@@ -22,44 +22,46 @@ namespace NSLoaderWrapper
 			//	loader.PlayEffect(1, 1, 1.9f, 1.0f, 1);
 			//loader.PlayEffect(1, 1, 1.9f, 1.0f, 1);
 			//bool toggle = true;
-			//	NSVR.NSVR_Plugin loader = new NSVR.NSVR_Plugin(@"C:\Users\NullSpace Team\Documents\NullSpace SDK 0.1.1\Assets\StreamingAssets\Haptics");
+				NSVR.NSVR_Plugin loader = new NSVR.NSVR_Plugin(@"C:\Users\NullSpace Team\Documents\NullSpace SDK 0.1.1\Assets\StreamingAssets\Haptics");
 
-		
 			//	Sequence s = new Sequence("ns.basic.click_click_click");
 			//s.CreateHandle(AreaFlag.All_Areas).Play();
 			//DefaultTimeProvider p = new DefaultTimeProvider();
 			//RandomGenerator g = new RandomGenerator();
-
+			while (true)
+			{
+				Console.WriteLine(loader.PollStatus());
+			}
 			//	g.GenerateNext(new CodeEffect(0f, "click", 0f, 1f, AreaFlag.None)).Play();
 
 			//	CodePattern pa = new CodePattern();
-		
-/*
-			CodeSequence seq = new CodeSequence();
-			seq.AddChild(new CodeEffect("click", 0f));
-			
-			var randomGen = new RandomGenerator();
 
-			for (int i = 0; i < 100; i++)
-			{
-				randomGen.GenerateNext(seq).Play();
-				System.Threading.Thread.Sleep(50);
-			}
+			/*
+						CodeSequence seq = new CodeSequence();
+						seq.AddChild(new CodeEffect("click", 0f));
+
+						var randomGen = new RandomGenerator();
+
+						for (int i = 0; i < 100; i++)
+						{
+							randomGen.GenerateNext(seq).Play();
+							System.Threading.Thread.Sleep(50);
+						}
+						*/
+
+			/*	CutaneousRabbit rabbit = new CutaneousRabbit(AreaFlag.Forearm_Left, AreaFlag.Upper_Arm_Left);
+				rabbit.SetParam("strength", 1.0);
+				rabbit.SetParam("frequency", 2.4);
+				GenericStrengthShader shader = new GenericStrengthShader();
+				ShaderProgram program = new ShaderProgram(rabbit, shader);
+				program.UseTimeProvider(p);
+				program.Execute();
+
+
+				Console.ReadLine();
+
+				program.Destroy();
 			*/
-			
-		/*	CutaneousRabbit rabbit = new CutaneousRabbit(AreaFlag.Forearm_Left, AreaFlag.Upper_Arm_Left);
-			rabbit.SetParam("strength", 1.0);
-			rabbit.SetParam("frequency", 2.4);
-			GenericStrengthShader shader = new GenericStrengthShader();
-			ShaderProgram program = new ShaderProgram(rabbit, shader);
-			program.UseTimeProvider(p);
-			program.Execute();
-			
-
-			Console.ReadLine();
-
-			program.Destroy();
-		*/
 			//handle1.Play();
 			//	Console.ReadLine();
 			//	var ha = seq.CreateHandle(AreaFlag.Chest_Left).Play();
