5f9377e9f1c43006a66ad0ec184c5f778cacae58
Casey Waldren <caseywaldren@gmail.com>
2017-11-15 12:38:49 -0800

    Add git versioning script

---
+++ build/NGitVersion/Generated/GlobalAssemblyInfo.h
@@ -0,0 +1,9 @@
+#define VERSION_COMA   1,0,0,122
+#define VERSION        "1.0.0.122"
+#define FULL_VERSION   "1.0.0.122, Hash 11ef6eb, BuildConfig RELEASE, HasLocalChange True"
+#define COMPANY        "NullSpace VR"
+#define PRODUCT        "Hardlight SDK"
+#define COPYRIGHT      "Copyright © 2017"
+#define TRADEMARK      ""
+#define CONFIGURATION  "RELEASE"
+#define CULTURE        ""
